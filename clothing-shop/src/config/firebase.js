8ef5a861fedf7a9e6f9eb3bf41398f802dcc56e7
Todor Krumov <baito6u@gmail.com>
2024-07-05 05:15:41 +0300

    install and config Firebase

---
+++ clothing-shop/src/config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBK3X2TMfSVODLu2ZMGe4YmrwI8PuT4eOc",
+  authDomain: "clothing-shop-c1115.firebaseapp.com",
+  projectId: "clothing-shop-c1115",
+  storageBucket: "clothing-shop-c1115.appspot.com",
+  messagingSenderId: "14124603285",
+  appId: "1:14124603285:web:72a1f76f657ea147e6c89c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
