38f425aa2560fd0c385dbb9ac7d1ae38ef8f4fc1
Todor Krumov <baito6u@gmail.com>
2024-07-09 06:41:04 +0300

    add some css to home and navbar

--- clothing-shop/src/components/Navbar.jsx
+++ clothing-shop/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png"
 
 export const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div className="navbox">
+    <div className="leftside">
+        <img src={logo} alt="" />
+    </div>
+    <div className="rightside">
+        <Link to={"/register"} className="navlinks">Register</Link>
+        <Link to={"/login"} className="navlinks">Login</Link>
+    </div>
+    
+    </div>;
+};
