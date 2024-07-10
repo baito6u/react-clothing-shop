d729fdc798b550bd0d3d2cc9900a2b0afd4b6d95
Todor Krumov <baito6u@gmail.com>
2024-07-10 06:38:28 +0300

    Update AddProducts.jsx

--- clothing-shop/src/addPrducts/AddProducts.jsx
+++ clothing-shop/src/addPrducts/AddProducts.jsx
@@ -1,21 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const AddProducts = () => {
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState(0);
+  const [productDescription, setProductDescription] = useState("");
+  const [productImg, setProductImg] = useState("");
+  const [error, setError] = useState("");
+
+  const addProduct = (e) => {
+    e.preventDefault();
+    console.log(productName, productPrice, productDescription, productImg);
+  }
+
   return (
-    <div className='container'>
+    <div className="container">
+      <br />
+      <h2>ADD PRODUCTS</h2>
+      <hr />
+      <form autoComplete="off" className="form-group" onSubmit={addProduct}>
+        <label htmlFor="product-name">Product Name</label>
+        <input
+          type="text"
+          className="form-control"
+          title="product-name"
+          required
+          onChange={(e) => setProductName(e.target.value)}
+          value={productName}
+        />
         <br />
-        <h2>ADD PRODUCTS</h2>
-        <hr />
-        <form autoComplete="off" className='form-group' >
-            <label htmlFor="product-name">Product Name</label>
-            <br />
-            <input type="text" className='form-control' required />
-            <label htmlFor="product-price">Product Price</label>
-            <input type="number" className='form-control' required />
-            <input type="file"  />
-            <br />
-            <button className='btn btn-success btn-md'>ADD PRODUCT</button>
-        </form>
-        </div>
-  )
-}
+        <label htmlFor="product-price">Product Price</label>
+        <input
+          type="number"
+          className="form-control"
+          title="product-price"
+          required
+          onChange={(e) => setProductPrice(e.target.value)}
+          value={productPrice}
+        />
+        <br />
+        <label htmlFor="product-description">Product Description</label>
+        <input
+          type="text"
+          className="form-control"
+          title="product-description"
+          required
+          onChange={(e) => setProductDescription(e.target.value)}
+          value={productDescription}
+        />
+        <br />
+        <label htmlFor="product-img">Product Image</label>
+        <input
+          type="text"
+          className="form-control"
+          title="product-img"
+          onChange={(e) => setProductImg(e.target.value)}
+          value={productImg}
+        />
+        <br />
+        <button type="submit" className="btn btn-success btn-md mybtn">ADD PRODUCT</button>
+      </form>
+      {/* {error && <span>{error}</span>} */}
+    </div>
+  );
+};
