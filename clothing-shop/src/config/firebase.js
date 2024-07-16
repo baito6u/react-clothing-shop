6e247a9a488654bfe3864e7371226941f815bcbd
Todor Krumov <baito6u@gmail.com>
2024-07-16 04:12:07 +0300

    Update firebase.js

--- clothing-shop/src/config/firebase.js
+++ clothing-shop/src/config/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import firebase from "firebase/compat/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBK3X2TMfSVODLu2ZMGe4YmrwI8PuT4eOc",
@@ -9,12 +9,12 @@ const firebaseConfig = {
   projectId: "clothing-shop-c1115",
   storageBucket: "clothing-shop-c1115.appspot.com",
   messagingSenderId: "14124603285",
-  appId: "1:14124603285:web:72a1f76f657ea147e6c89c"
+  appId: "1:14124603285:web:72a1f76f657ea147e6c89c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = firebase.storage(app);
+export const fs = getFirestore(app);
+export const storage = getStorage(app);
