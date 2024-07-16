49e96ebb73e6c35fb5ef71c6fe30d5e78040451f
Todor Krumov <baito6u@gmail.com>
2024-07-16 04:11:55 +0300

    Update AddProducts.jsx

--- clothing-shop/src/addPrducts/AddProducts.jsx
+++ clothing-shop/src/addPrducts/AddProducts.jsx
@@ -1,65 +1,146 @@
 import React, { useState } from "react";
+import { fs, storage } from "../config/firebase";
 
 export const AddProducts = () => {
-  const [productName, setProductName] = useState("");
-  const [productPrice, setProductPrice] = useState(0);
-  const [productDescription, setProductDescription] = useState("");
-  const [productImg, setProductImg] = useState("");
-  const [error, setError] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+  const [imageError, setImageError] = useState("");
 
-  const addProduct = (e) => {
+  const [uploadError, setUploadError] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+
+  const types = ["image/png", "image/jpeg", "image/PNG", "image/jpg"];
+
+  const handleImg = (e) => {
+    let selectedFile = e.target.files[0];
+    if (selectedFile) {
+      if (selectedFile && types.includes(selectedFile.type)) {
+        setImage(selectedFile);
+        setImageError("");
+      } else {
+        setImage(null);
+        setImageError("Please select a valid image type (png or jpg)");
+      }
+    } else {
+      console.log("select your file");
+    }
+  };
+
+  const handleAddProducts = (e) => {
     e.preventDefault();
-    console.log(productName, productPrice, productDescription, productImg);
-  }
+    const uploadTask = storage.ref(`product-images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(progress);
+      },
+      (err) => setUploadError(err.message),
+      () => {
+        storage
+          .ref("product-images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            fs.collection("Products")
+              .add({
+                title,
+                description,
+                price: Number(price),
+                url,
+              })
+              .then(() => {
+                setSuccessMsg("Product added successfully");
+                setTitle("");
+                setDescription("");
+                setPrice("");
+                document.getElementById("file").value = "";
+                setUploadError("");
+                setImageError("");
+                setTimeout(() => {
+                  setSuccessMsg("");
+                }, 3000);
+              })
+              .catch((err) => setUploadError(err.message));
+          });
+      }
+    );
+  };
 
   return (
     <div className="container">
-      <br />
-      <h2>ADD PRODUCTS</h2>
-      <hr />
-      <form autoComplete="off" className="form-group" onSubmit={addProduct}>
-        <label htmlFor="product-name">Product Name</label>
+      <br></br>
+      <br></br>
+      <h1>Add Products</h1>
+      <hr></hr>
+      {successMsg && (
+        <>
+          <div className="success-msg">{successMsg}</div>
+          <br></br>
+        </>
+      )}
+      <form
+        autoComplete="off"
+        className="form-group"
+        onSubmit={handleAddProducts}
+      >
+        <label>Product Title</label>
         <input
           type="text"
           className="form-control"
-          title="product-name"
           required
-          onChange={(e) => setProductName(e.target.value)}
-          value={productName}
-        />
-        <br />
-        <label htmlFor="product-price">Product Price</label>
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        ></input>
+        <br></br>
+        <label>Product Description</label>
+        <input
+          type="text"
+          className="form-control"
+          required
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></input>
+        <br></br>
+        <label>Product Price</label>
         <input
           type="number"
           className="form-control"
-          title="product-price"
           required
-          onChange={(e) => setProductPrice(e.target.value)}
-          value={productPrice}
-        />
-        <br />
-        <label htmlFor="product-description">Product Description</label>
+          onChange={(e) => setPrice(e.target.value)}
+          value={price}
+        ></input>
+        <br></br>
+        <label>Upload Product Image</label>
         <input
-          type="text"
+          type="file"
+          id="file"
           className="form-control"
-          title="product-description"
           required
-          onChange={(e) => setProductDescription(e.target.value)}
-          value={productDescription}
-        />
-        <br />
-        <label htmlFor="product-img">Product Image</label>
-        <input
-          type="text"
-          className="form-control"
-          title="product-img"
-          onChange={(e) => setProductImg(e.target.value)}
-          value={productImg}
-        />
-        <br />
-        <button type="submit" className="btn btn-success btn-md mybtn">ADD PRODUCT</button>
+          onChange={handleImg}
+        ></input>
+        {imageError && (
+          <>
+            <br></br>
+            <div className="error-msg">{imageError}</div>
+          </>
+        )}
+        <br></br>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <button type="submit" className="btn btn-success btn-md">
+            SUBMIT
+          </button>
+        </div>
       </form>
-      {/* {error && <span>{error}</span>} */}
+      {uploadError && (
+        <>
+          <br></br>
+          <div className="error-msg">{uploadError}</div>
+        </>
+      )}
     </div>
   );
 };
