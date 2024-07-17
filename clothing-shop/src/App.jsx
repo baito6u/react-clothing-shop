544e3fff90bdd2e3ba3faff686a96980b41af4a9
Todor Krumov <baito6u@gmail.com>
2024-07-17 20:16:43 +0300

    Update App.jsx

--- clothing-shop/src/App.jsx
+++ clothing-shop/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import { Home } from "./components/home/Home";
 import { AddProducts } from "./components/addPrducts/AddProducts";
 
