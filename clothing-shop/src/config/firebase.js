781501e06485d40c0456324c62bb2e69ffb023ee
Todor Krumov <baito6u@gmail.com>
2024-07-17 18:45:18 +0300

    Update firebase.js

--- clothing-shop/src/config/firebase.js
+++ clothing-shop/src/config/firebase.js
@@ -3,6 +3,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+import firebase from "firebase/compat/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBK3X2TMfSVODLu2ZMGe4YmrwI8PuT4eOc",
   authDomain: "clothing-shop-c1115.firebaseapp.com",
@@ -12,9 +17,17 @@ const firebaseConfig = {
   appId: "1:14124603285:web:72a1f76f657ea147e6c89c",
 };
 
-// Initialize Firebase
+//Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const fs = getFirestore(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// firebase.initializeApp(firebaseConfig);
+
+// export const auth = firebase.getAuth();
+// export const db = firebase.firestore();
+// export const storage = firebase.storage();
+
+
